918c69e012b0fb6a5539aed34ac2b3ab1198c36f
towcrash <fernando.vega@alumnos.ucn.cl>
2025-01-07 21:51:24 -0300

    feat: add angular base

---
+++ Catedra3/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'Catedra3';
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
